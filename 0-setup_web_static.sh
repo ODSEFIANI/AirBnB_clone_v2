2439b8e261e332dfbf734d16c1b9ece3e623cd44
AymanAegon <ayo12ammar24@gmail.com>
2023-10-05 07:39:23 -0700

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,9 +9,9 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 page="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
-echo $page | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu /data/
+echo "$page" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu /data/
 str="\\\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "22i $str" /etc/nginx/sites-available/default
+sudo sed -i "35i $str" /etc/nginx/sites-available/default
 sudo service nginx restart
